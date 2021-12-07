36d00fbfd625e868eec2c6015a191929eb57f1a9
Qingfei Wei <gikeihi@e-business.co.jp>
2021-12-07 14:55:50 +0900

    修改readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # k8s
+## 各个工作节点都要安装
+```bash
+curl -sSL https://raw.githubusercontent.com/gikeihi/k8s/main/install_kubelet.sh | sh -s 1.22.3
+```
+## Master节点初始化
+```bash
+# 替换 x.x.x.x 为 master 节点的内网IP
+export MASTER_IP=192.168.11.201
+echo "${MASTER_IP}    cluster-endpoint" >> /etc/hosts
+curl -sSL https://raw.githubusercontent.com/gikeihi/k8s/main/init_master.sh | sh -s 1.22.3
+```
--- init_master.sh
+++ init_master.sh
@@ -1,24 +1,8 @@
 #!/bin/bash
 
 # 只在 master 节点执行
-# 替换 x.x.x.x 为 master 节点的内网IP
-# export 命令只在当前 shell 会话中有效，开启新的 shell 窗口后，如果要继续安装过程，请重新执行此处的 export 命令
-export MASTER_IP=192.168.11.201
-# 替换 apiserver.demo 为 您想要的 dnsName
-export APISERVER_NAME=cluster-endpoint
-# Kubernetes 容器组所在的网段，该网段安装完成后，由 kubernetes 创建，事先并不存在于您的物理网络中
-export POD_SUBNET=192.168.0.0/16
-echo "${MASTER_IP}    ${APISERVER_NAME}" >> /etc/hosts
 # 脚本出错时终止执行
 set -e
-
-if [ ${#POD_SUBNET} -eq 0 ] || [ ${#APISERVER_NAME} -eq 0 ]; then
-  echo -e "\033[31;1m请确保您已经设置了环境变量 POD_SUBNET 和 APISERVER_NAME \033[0m"
-  echo 当前POD_SUBNET=$POD_SUBNET
-  echo 当前APISERVER_NAME=$APISERVER_NAME
-  exit 1
-fi
-
 
 # 查看完整配置选项 https://godoc.org/k8s.io/kubernetes/cmd/kubeadm/app/apis/kubeadm/v1beta2
 rm -f ./kubeadm-config.yaml
@@ -27,10 +11,10 @@ cat <<EOF > ./kubeadm-config.yaml
 apiVersion: kubeadm.k8s.io/v1beta2
 kind: ClusterConfiguration
 kubernetesVersion: v${1}
-controlPlaneEndpoint: "${APISERVER_NAME}:6443"
+controlPlaneEndpoint: "cluster-endpoint:6443"
 networking:
   serviceSubnet: "10.96.0.0/16"
-  podSubnet: "${POD_SUBNET}"
+  podSubnet: "192.168.0.0/16"
   dnsDomain: "cluster.local"
 dns:
   type: CoreDNS
